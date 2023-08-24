ad7df14675658eab08f019fee95ee3f51941cc93
Hugo Dias <hugomrdias@gmail.com>
2023-08-24 15:56:49 +0100

    feat: support for nextjs and sveltekit

--- examples/sveltekit/vite.config.ts
+++ examples/sveltekit/vite.config.ts
@@ -1,13 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import wasm from 'vite-plugin-wasm'
 
 export default defineConfig({
   build: {
     target: 'esnext'
   },
+  resolve: {
+    conditions: ['import', 'module', 'browser', 'default']
+  },
   plugins: [sveltekit(), wasm()],
-  test: {
-    include: ['src/**/*.{test,spec}.{js,ts}']
-  }
 })
